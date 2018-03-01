e6067bf12a8624c18cd4edcd72224d58a66e0d8a
emilyvandegrift <36965068+emilyvandegrift@users.noreply.github.com>
2018-03-01 11:11:33 -0800

    add index column to oceania

--- analysis.py
+++ analysis.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 """
 Spyder Editor
 
@@ -8,6 +8,6 @@ This is a temporary script file.
 import pandas as pd
 
 dat = pd.read_csv('data/gapminder_gdp_europe.csv')
-oceania = pd.read_csv('data/gapminder_gdp_oceania.csv')
+oceania = pd.read_csv('data/gapminder_gdp_oceania.csv' , index_col = 'country')
 asia = pd.read_csv('data/gapminder_gdp_asia.csv')
 
