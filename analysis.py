5bba7a075a91113a151c086c4a1faab64f031f0f
metal <emily.vandegrift@wsu.edu>
2018-03-01 10:51:57 -0800

    got caught up

--- analysis.py
+++ analysis.py
@@ -8,5 +8,5 @@ This is a temporary script file.
 import pandas as pd
 
 dat = pd.read_csv('data/gapminder_gdp_europe.csv')
-dat_1 = pd.read_csv('data/gapminder_gdp_oceania.csv')
+oceania = pd.read_csv('data/gapminder_gdp_oceania.csv')
 
